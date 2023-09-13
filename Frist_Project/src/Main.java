ee455fe0f87aaf541743eecf61041d4f9b207800
kian-darrington <90269026+kian-darrington@users.noreply.github.com>
2023-09-13 10:55:05 -0700

    Started numberdle

--- Frist_Project/src/Main.java
+++ Frist_Project/src/Main.java
@@ -2,21 +2,55 @@
 // then press Enter. You can now see whitespace characters in your code.
 import java.util.*;
 public class Main {
+    public static int runFunction(int x) {
+        return x * x + - 5;
+    }
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!\n");
+        System.out.println("Guess this 5 digit number!");
+        int guessnum = 1;
+        Random rand = new Random();
+        int randomNum = rand.nextInt(100000);
+        int guess = console.nextInt();
+        /*
+        Scanner console = new Scanner(System.in);
+        int xmin = -10;
+        int xmax = 10;
+        int ymin = -10;
+        int ymax = 10;
+        char t = 9607;
+        char yaxis = 9553;
+        char center = 9580;
+        char xaxis = 9552;
+        for(int ylevel = ymax; ylevel >= ymin; ylevel--)
+        {
+            for (int xlevel = xmin; xlevel <= xmax; xlevel++)
+            {
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-       double f = 111.22222;
-       char t = 219;
-       for (int i = 32; i < 256; i++) {
+                if (runFunction(xlevel) == ylevel)
+                    System.out.print(t);
+                else if (xlevel == 0 && ylevel == 0)
+                    System.out.print(center);
+                else if (ylevel == 0)
+                    System.out.print(xaxis);
+                else if (xlevel == 0)
+                    System.out.print(yaxis);
+                else
+                    System.out.print(' ');
+            }
+            System.out.println();
+        }
+
+       for (int i = 32; i < 10192; i++) {
            t = (char)i;
-           System.out.print(i + " : " + t);
-           if ((i - 32) % 6 == 0) {
-
+           System.out.print(i + ": " + t);
+           if ((i - 31) % 6 != 0) {
+                System.out.print('\t');
+           }
+           else {
+               System.out.println();
            }
        }
+        */
     }
 }
