8bd1b4986ff356895af19f242dd374f447ec747c
kian-darrington <90269026+kian-darrington@users.noreply.github.com>
2023-09-20 10:55:01 -0700

    Changed Numberdle to MasterMind
    A lot of code adaptation, still need to work on scoring

--- Frist_Project/src/Main.java
+++ Frist_Project/src/Main.java
@@ -6,24 +6,63 @@ public class Main {
         return x * x + - 5;
     }
     public static void main(String[] args) {
+        //System initialization and Title for user, code is for a MasterMind simulator
         Scanner console = new Scanner(System.in);
-        System.out.println("Guess this 5 digit number!");
-        int guessnum = 1;
+        final int NUMBER_LENGTH = 4;
+        final int NUMBER_AMOUNT = 6;
+        System.out.println("Welcome to MasterMind!\nGuesses should have "+NUMBER_LENGTH+" digits with numbers ranging from 0 - " + (NUMBER_AMOUNT - 1));
         Random rand = new Random();
+
+        //Establishes number of guesses and initialized the random answer and ensures that it has the required digits
+        int guessnum = 1;
         String randomNum = "";
-        randomNum += rand.nextInt(100000);
+        for (int i = 0; i < NUMBER_LENGTH; i++)
+            randomNum += rand.nextInt(NUMBER_AMOUNT);
+
+        //Initialized first guess and error checking
+        String guess = "";
         while (true) {
-            String guess = "" + console.nextInt();
-            if (guess.length() == 5)
+            guess = console.nextLine();
+            if (guess.length() == NUMBER_LENGTH)
                 break;
             else
-                System.out.println("Please type in a number with 5 digits");
+                System.out.println("Please type in a number with "+ NUMBER_LENGTH +" digits");
         }
-        while (guess != randomNum){
-            System.out.println("Incorrect");
+
+
+        int numberCorrect;
+        while (!guess.equals(randomNum)){
+            numberCorrect = 0;
+            for (int i = 0; i < NUMBER_LENGTH; i++) {
+                char currentRealLetter = randomNum.charAt(i);
+                char currentGuessLetter =  guess.charAt(i);
+                if (currentGuessLetter == currentRealLetter)
+                {
+                    numberCorrect++;
+                }
+                else
+                {
+                    for (int o = 0; o < NUMBER_LENGTH; o++){
+                        if (o == i)
+                            continue;
+                        currentGuessLetter = guess.charAt(o);
+
+                    }
+                }
+            }
+            System.out.println("You got " + numberCorrect + " pins right");
+            System.out.println("That is incorrect, try again!");
             guessnum++;
-            int[] correct;
+            while (true) {
+                guess = console.nextLine();
+                if (guess.length() == NUMBER_LENGTH)
+                    break;
+                else
+                    System.out.println("Please type in a number with "+NUMBER_LENGTH+" digits");
+            }
         }
+        System.out.println("You are correct!");
+        System.out.print("It took you " + guessnum + " guesses");
         /*
         Scanner console = new Scanner(System.in);
         int xMin = -10;
