44be28b07c024b5e3a4b50cbde58dcced178d0a9
kian-darrington <90269026+kian-darrington@users.noreply.github.com>
2023-12-04 10:39:56 -0800

    More class work

--- Frist_Project/src/MazeMaker.java
+++ Frist_Project/src/MazeMaker.java
@@ -1,6 +1,49 @@
-import java.util.*;
 public class MazeMaker {
     public static final int X_ROOMS = 4;
     public static final int Y_ROOMS = 4;
-
+    private static Room[][] rooms = new Room[X_ROOMS][Y_ROOMS];
+    public static final int N = 0, E = 1, S = 2, W = 3;
+    void setUpRooms(){
+        for(int i =0; i < X_ROOMS; i++){
+            for (int o = 0; o < Y_ROOMS; o++){
+                rooms[i][o].setCord(i, o);
+            }
+        }
+    }
+    public void alterSurrounding(boolean[] t, int x, int y)
+    {
+        boolean[] temp = rooms[x][y].getMove();
+        if (x != 0)
+            rooms[x - 1][y].changeMove(temp[W], E);
+        if (x < X_ROOMS)
+            rooms[x + 1][y].changeMove(temp[E], W);
+        if (y != 0)
+            rooms[x][y - 1].changeMove(temp[S], N);
+        if (y < Y_ROOMS)
+            rooms[x][y + 1].changeMove(temp[N], S);
+    }
+    public void alterSurrounding(boolean t, int direction, int x, int y){
+        if (x != 0 && direction == W)
+            rooms[x - 1][y].changeMove(t, E);
+        if (x < X_ROOMS && direction == E)
+            rooms[x + 1][y].changeMove(t, W);
+        if (y != 0 && direction == S)
+            rooms[x][y - 1].changeMove(t, N);
+        if (y < Y_ROOMS && direction == N)
+            rooms[x][y + 1].changeMove(t, S);
+    }
+    public void changeMove(boolean t, int direction, int x, int y){
+        rooms[x][y].changeMove(t, direction);
+        alterSurrounding(t, direction, x, y);
+    }
+    public void changeMove(boolean[] t, int x, int y){
+        rooms[x][y].changeMove(t);
+        alterSurrounding(t, x, y);
+    }
+    public Room[][] getMaze(){
+        return rooms;
+    }
+    MazeMaker(){
+        setUpRooms();
+    }
 }
--- Frist_Project/src/Room.java
+++ Frist_Project/src/Room.java
@@ -3,11 +3,37 @@ public class Room {
     final int Y_ROOM = MazeMaker.Y_ROOMS;
     int xCord;
     int yCord;
-    boolean[] accessable = new boolean[4]; //True or false for N, E, S, W
+    public static final int N = 0, E = 1, S = 2, W = 3;
+    boolean[] canMove = new boolean[4];
+    //True or false for N, E, S, W
     Room(int x, int y, boolean[] t)
     {
         xCord = x;
         yCord = y;
-        accessable = t;
+        canMove = t;
     }
+    public void setCord(int x, int y){
+        xCord = x;
+        yCord = y;
+    }
+    void edgeCheck()
+    {
+        if (xCord == 0)
+            canMove[E] = false;
+        else if (xCord == X_ROOM -1)
+            canMove[W] = false;
+        if (yCord == 0)
+            canMove[N] = false;
+        if (yCord == Y_ROOM -1)
+            canMove[S] = false;
+    }
+    void changeMove(boolean[] t){
+        canMove = t;
+        edgeCheck();
+    }
+    void changeMove(boolean t, int direction){
+        canMove[direction] = t;
+        edgeCheck();
+    }
+    public boolean[] getMove() { return canMove; }
 }
