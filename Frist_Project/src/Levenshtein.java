b6abec110f74512051354bc55f6ab5c22f7b79fa
kian-darrington <90269026+kian-darrington@users.noreply.github.com>
2024-02-15 10:54:57 -0800

    Created a massive brute force system

--- Frist_Project/src/Levenshtein.java
+++ Frist_Project/src/Levenshtein.java
@@ -5,31 +5,36 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Levenshtein {
-    public static final ArrayList<String> dictionary = getWords();
+    public static final String FILENAME = "src/dictionarySortedLength.txt";
+    public static final ArrayList<Word> dictionary = getWords();
     private static final Scanner console = new Scanner(System.in);
-    public static ArrayList<String> getWords() {
-        ArrayList<String> words = new ArrayList<>();
+    public static ArrayList<Word> getWords() {
+        ArrayList<Word> words = new ArrayList<>();
         Scanner f;
         try {
-            f = new Scanner(new File("src/dictionarySortedLength.txt"));
+            f = new Scanner(new File(FILENAME));
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        int i = 0;
         while (f.hasNext()) {
-            words.add(f.next().toLowerCase());
+            words.add(new Word(f.next().toLowerCase(), i));
+            i++;
         }
         return words;
     }
-    public static ArrayList<byte[]> getAlphaWords() {
-        ArrayList<byte[]> words = new ArrayList<>();
+    public static ArrayList<String> getWordsStrings() {
+        ArrayList<String> words = new ArrayList<>();
         Scanner f;
         try {
-            f = new Scanner(new File("src/dictionaryCatDog.txt"));
+            f = new Scanner(new File(FILENAME));
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        int i = 0;
         while (f.hasNext()) {
-            words.add(f.next().toLowerCase().getBytes());
+            words.add(f.next().toLowerCase());
+            i++;
         }
         return words;
     }
@@ -49,56 +54,59 @@ public class Levenshtein {
         System.out.println("Input your two words:");
         String word1 = console.nextLine();
         String word2 = console.nextLine();
-        while (!dictionary.contains(word1) || !dictionary.contains(word2)){
+        ArrayList<String> temp = getWordsStrings();
+        while (!temp.contains(word1) || !temp.contains(word2)){
             System.out.println("One or both of your words is not found in dictionary");
             System.out.println("Input another word");
-            if (!dictionary.contains(word1))
+            if (!temp.contains(word1))
                 word1 = console.nextLine();
-            else if (dictionary.contains(word2))
+            else if (temp.contains(word2))
                 word2 = console.nextLine();
         }
-        findDistance(word1, word2);
+        System.out.println("Both words are A OK");
+        System.out.println(findLocation(word1)+ " " + findLocation(word2));
+        ArrayList<ArrayList<Word>> paths = findDistance(word1, word2);
     }
-    static boolean findDistance(String word1, String word2){
-        ArrayList<String> previousChoices = new ArrayList<>();
-        previousChoices.add(word1);
-        int bigStart = startingIndex(word1.length(), 0, dictionary.size() - 1);
-        int bigFinish = endingIndex(word1.length(), bigStart, dictionary.size() - 1);
-        ArrayList<String> possible = new ArrayList<>(dictionary.subList(bigStart, bigFinish + 1));
-        int first = firstIndex(word1.length(), bigStart, possible.size() - 1, possible);
-        int last = lastIndex(word1.length(), first, possible.size() - 1, possible);
-        System.out.println(first +" "+ last + ", " + bigStart + " " + bigFinish);
-        return false;
-    }
-    ArrayList<String> binarySearchLength(int length){
+    static ArrayList<ArrayList<Word>> findDistance(String word1, String word2){
         return null;
     }
-    static int startingIndex(int length, int start, int finish){
+    int[] findDistanceRecurse(int word1, int word2, int currentShortest, int[] choices){
+        int[] previousChoices = new int[1];
+        if (choices != null) {
+            previousChoices = new int[choices.length + 1];
+            for (int i = 0; i < choices.length; i++)
+                previousChoices[i] = choices[i];
+            previousChoices[previousChoices.length - 1] = word1;
+        } else
+            previousChoices[0] = word1;
+        ArrayList<Integer> possible = new ArrayList<>();
+        for (int i = lengthStarts[word1 - 2]; i < lengthStarts[word1]; i++)
+            if (misMatch(dictionary.get(word1).toString(), dictionary.get(i).toString()))
+                possible.add(i);
+        if (possible.contains(word2))
+            return previousChoices;
+        else{
+            for (int i : previousChoices)
+                possible.remove((Integer) i);
+        }
+        if (possible.isEmpty())
+            return null;
+        else{
+            for (int i : possible) {
+                int[] temp = findDistanceRecurse(i, word2, currentShortest, previousChoices);
+                if (temp != null)
+                    return temp;
+            }
+
+        }
+        return null;
+    }
+    static int firstIndex(int length, int start, int finish, ArrayList<Word> current){
         int mid = (start + finish) / 2;
         if (mid == 0)
             return 0;
-        if ((dictionary.get(mid).length() == dictionary.get(mid - 1).length() + 1) && dictionary.get(mid).length() == length - 1) {
-            return mid;
-        } else if (dictionary.get(mid).length() > length - 2) {
-            return startingIndex(length, start, mid);
-        } else
-            return startingIndex(length, mid, finish);
-    }
-    static int endingIndex(int length, int start, int finish){
-        int mid = (start + finish) / 2;
-        if (mid + 1 == dictionary.size()-1)
-            return mid + 1;
-        if ((dictionary.get(mid).length() == dictionary.get(mid + 1).length() - 1) && dictionary.get(mid).length() == length + 1) {
-            return mid;
-        } else if (dictionary.get(mid).length() > length + 1) {
-            return endingIndex(length, start, mid);
-        } else
-            return endingIndex(length, mid, finish);
-    }
-    static int firstIndex(int length, int start, int finish, ArrayList<String> current){
-        int mid = (start + finish) / 2;
-        if (mid == 0)
-            return 0;
+        else if (finish - start == 1 && length != current.get(start).length())
+            return -1;
         if ((current.get(mid).length() == current.get(mid - 1).length() + 1) && current.get(mid).length() == length) {
             return mid;
         } else if (current.get(mid).length() > length - 1) {
@@ -106,18 +114,61 @@ public class Levenshtein {
         } else
             return firstIndex(length, mid, finish, current);
     }
-    static int lastIndex(int length, int start, int finish, ArrayList<String> current){
-        int mid = (start + finish) / 2;
-        if (mid + 1 == current.size() - 1)
-            return mid + 1;
-        if ((current.get(mid).length() == current.get(mid + 1).length() - 1) && current.get(mid).length() == length) {
-            return mid;
-        } else if (current.get(mid).length() > length) {
-            return lastIndex(length, start, mid, current);
-        } else
-            return lastIndex(length, mid, finish, current);
+    static boolean misMatch(String word1, String word2) {
+        int difference = 0;
+        byte[] w1 = word1.getBytes(), w2 = word2.getBytes();
+        if (word1.length() == word2.length()) {
+            for (int i = 0; i < w1.length; i++) {
+                if (w1[i] != w2[i]) {
+                    difference++;
+                    if (difference > 1)
+                        return false;
+                }
+            }
+        }
+        else
+        {
+            if (word2.length() > word1.length()) {
+                byte[] temp = w1;
+                w1 = w2;
+                w2 = temp;
+            }
+            int count = 0;
+            for (int i = 0; i < w1.length; i++) {
+                if (count < w2.length) {
+                    if (w1[i] != w2[count]) {
+                        difference++;
+                        if (difference > 1)
+                            return false;
+                    } else
+                        count++;
+                }
+                else {
+                    difference++;
+                    if (difference > 1)
+                        return false;
+                }
+            }
+        }
+        return difference != 0;
     }
-    int compareStrings(String word1, String word2){
-        return 0;
+    static int findLocation(String word){
+        int start = lengthStarts[word.length() - 1], end = lengthStarts[word.length()];
+        for (int i = start; i < end; i++){
+            if (word.equals(dictionary.get(i).toString()))
+                return i;
+        }
+        return -1;
     }
+}
+class Word{
+    String word = null;
+    int location;
+    Word(String w, int l){
+        word = w;
+        location = l;
+    }
+    int length() {return word.length();}
+    int getLocation() {return location;}
+    public String toString() {return word;}
 }
