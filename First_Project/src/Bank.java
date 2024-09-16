09683bd2529440828ec896579bb1f23719716316
kian-darrington <90269026+kian-darrington@users.noreply.github.com>
2024-09-16 13:09:43 -0700

    Finished the player game

--- First_Project/src/Bank.java
+++ First_Project/src/Bank.java
@@ -7,7 +7,8 @@ import java.util.*;
  */
 public class Bank {
     public static Random rand = new Random();
-    class gameValues {
+    static Scanner console = new Scanner(System.in);
+    static class gameValues {
         public int roundNum = 0;
         public int score = 0;
     }
@@ -18,13 +19,14 @@ public class Bank {
     // whether to bank or not
     static boolean playRound(gameValues game){
         game.roundNum++;
-        System.out.println("Round " + game.roundNum + ":");
+        System.out.println("Roll " + game.roundNum + ":");
         int[] dice = getDice();
         int sum = dice[0] + dice[1];
         System.out.println("Rolled a " + dice[0] + " and a " + dice[1]);
         if (game.roundNum > 3){
             if (sum == 7) {
-                System.out.println("Round Over!");
+                System.out.println("Round Over!\n");
+                console.next();
                 return false;
             }
             else if (dice[0] == dice[1])
@@ -38,23 +40,67 @@ public class Bank {
             else
                 game.score += sum;
         }
-        System.out.println("The round score is: " + game.score);
+        System.out.println("The current pot is: " + game.score + "\n");
         return true;
     }
     static int findMax(int[] array){
         int CurrentMax = Integer.MIN_VALUE;
         int topIndex = 0;
         for (int i = 0; i < array.length; i++){
-            
+            if(array[i] > CurrentMax){
+                topIndex = i;
+                CurrentMax = array[i];
+            }
         }
+        return topIndex;
     }
     static String playGame(int playerNum, int roundNum){
         int[] playerScores = new int[playerNum];
         for (int count = 0; count < roundNum; count++){
-
+            System.out.println("ROUND " + (count + 1) + "!!!!!\n");
+            boolean stillGoing = true;
+            boolean[] banked = new boolean[playerNum];
+            gameValues game = new gameValues();
+            for (int i = 0; i < 3; i++)
+                stillGoing = playRound(game);
+            while (stillGoing){
+                for (int i = 0; i < playerNum; i++){
+                    if (banked[i])
+                        continue;
+                    System.out.println("Player " + i + " would you like to bank? (y, n)");
+                    char ans = console.next().toLowerCase().toCharArray()[0];
+                    if (ans == 'y') {
+                        banked[i] = true;
+                        playerScores[i] += game.score;
+                    }
+                    else if (ans == 'n')
+                        continue;
+                    else{
+                        System.out.println("Bro type in the proper input");
+                        i--;
+                    }
+                }
+                stillGoing = hasFalse(banked);
+                if (stillGoing)
+                    stillGoing = playRound(game);
+            }
+            printScores(playerScores);
         }
+        int winner = findMax(playerScores);
+        return "Player " + winner +" won with " + playerScores[winner] + " points!";
+    }
+    static boolean hasFalse(boolean[] array){
+        for(int i = 0; i < array.length; i++)
+            if (!array[i])
+                return true;
+        return false;
+    }
+    static void printScores(int[] scores){
+        for (int i = 0; i < scores.length; i++)
+            System.out.println("Player " + i + " has " + scores[i] + " points");
+        System.out.println();
     }
     public static void main(String[] args){
-        System.out.println(playGame(1, 10));
+        System.out.println(playGame(2, 10));
     }
 }
